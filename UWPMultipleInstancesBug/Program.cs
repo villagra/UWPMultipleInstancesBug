7831c0f99f7a04dc6b6a73945f829bdcd44127ac
Jose <villagra@gmail.com>
2020-04-27 09:27:04 +0200

    Initial

---
+++ UWPMultipleInstancesBug/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation.Metadata;
+
+namespace UWPMultipleInstancesBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (ApiInformation.IsTypePresent("Windows.ApplicationModel.AppInstance"))
+            {
+                IActivatedEventArgs activatedArgs = AppInstance.GetActivatedEventArgs();
+
+                if (AppInstance.RecommendedInstance != null)
+                {
+                    AppInstance.RecommendedInstance.RedirectActivationTo();
+                }
+                else
+                {
+
+                    var instance = AppInstance.GetInstances().LastOrDefault();
+                    if (instance == null || activatedArgs is Windows.ApplicationModel.Activation.LaunchActivatedEventArgs)
+                    {
+                        // If we successfully registered this instance, we can now just
+                        // go ahead and do normal XAML initialization.
+                        AppInstance.FindOrRegisterInstanceForKey(Guid.NewGuid().ToString());
+                        global::Windows.UI.Xaml.Application.Start((p) => new App());
+                    }
+                    else
+                    {
+                        instance.RedirectActivationTo();
+                    }
+                }
+            }
+            else
+            {
+                global::Windows.UI.Xaml.Application.Start((p) => new App());
+            }
+        }
+    }
+}
